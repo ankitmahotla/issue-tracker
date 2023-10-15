552c21be39401760e835fbbafea66133c78070a8
ankitmahotla <ankitmahotla45@gmail.com>
2023-10-15 12:34:16 +0530

    Implemented Client Side Validation

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+export default function NewIssuePage() {
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+
+  return (
+    <div className="max-w-xl mb-5">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (e) {
+            setError("An unexpected error occurred.");
+          }
+        })}
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
+  );
+}
--- app/issues/page.jsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@radix-ui/themes";
-
-export default function page() {
-  return (
-    <div>
-      <Button>New Issue</Button>
-    </div>
-  );
-}
